1a5bad37ad30ab8258c1267c072aa170193227a6
Andy Fisher <andy-fisher@yandex.ru>
2023-02-02 12:54:17 +0300

    Add Drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:demo_shop/screens/orders_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Hello!'),
+            automaticallyImplyLeading: true,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Orders'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
